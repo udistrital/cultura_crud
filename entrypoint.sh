53f8c09b06508e73efbb388894f8e1804d1dc13c
Mafe Avendaño <mafe.ud@gmail.com>
2024-05-08 23:20:42 -0500

    fix: entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 set -u
 set -o pipefail
 
-if [ -n "${CULTURA_STORE:-}" ]; then
+if [ -n "${PARAMETER_STORE:-}" ]; then
   export CULTURA_CRUD_PGUSER="$(aws ssm get-parameter --name /${PARAMETER_STORE}/cultura_crud/db/username --output text --query Parameter.Value)"
   export CULTURA_CRUD_PGPASS="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/cultura_crud/db/password --output text --query Parameter.Value)"
 fi
